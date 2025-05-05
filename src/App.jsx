1003a533c548790f9c1f0aaa13a17d7cc1add2e5
lesedi thibile <lesedi.xys11@gmail.com>
2025-05-05 16:17:25 +0200

    Chore: removed default styles and add tailwind for styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ function App() {
   const [value, setValue] = useState(0)
 
   return (
-    <button onClick={() => setValue(value + 1)}>{value}</button>
+    <>
+      <p className='text-center text-3xl'>Counter: {value}</p>
+      <button className='border border-b-cyan-950 px-3 py-1 rounded-xs cursor-pointer' onClick={() => setValue(value + 1)}
+        >Press Me
+      </button>
+    </>
   )
 }
 
